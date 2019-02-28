d462a8f41bc0ec8b33da44e1f79cb14cb60e39bb
cvoges12 <38054771+cvoges12@users.noreply.github.com>
2019-02-27 21:50:35 -0600

    Update network.nix

--- network.nix
+++ network.nix
@@ -5,9 +5,9 @@
     hostName = "annet-x220";
     networkmanager.enable = true;
     firewall = {
+      enable = true;
       allowedTCPPorts = [ 22 53 67 68 80 443 ];
       allowedUDPPorts = [ 22 53 67 68 80 433 ];
-      enable = true;
     };
   };
 }
