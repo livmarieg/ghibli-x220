0f59460e445721d9a282a140c338d3216483cfb2
Clayton Voges <vogesclayton@gmail.com>
2019-03-01 22:36:48 -0600

    add wg options but not enabled

--- network.nix
+++ network.nix
@@ -10,5 +10,15 @@
       allowedUDPPorts = [ 22 53 67 68 80 433 ];
     };
     nameservers = [ "1.1.1.1" ];
+    #wireguard.interfaces.wg0 = {
+    #  ips = [ "" ];
+    #  listenPort = null;
+    #  peers = [
+    #    { allowedIPs = [ "" ];
+    #      endpoint = "";
+    #      publicKey = "";}
+    #  ];
+    #  privateKeyFile = "";
+    #};
   };
 }
