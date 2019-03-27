664341a9af970f5399f74930f366414fd88ad559
Clayton Voges <38054771+cvoges12@users.noreply.github.com>
2019-03-27 09:57:31 -0500

    Update security.nix
    Moved common configs to annet-common

--- security.nix
+++ security.nix
@@ -2,17 +2,6 @@
 
 { 
   security = {
-    sudo.enable = true;
-    apparmor.enable = true;
-    audit.enable = true;
-    auditd.enable = true;
-    dhparams = {
-      enable = true;
-      stateful = true;
-    };
-    hideProcessInformation = true;
-    polkit.enable = true;
-    rngd.enable = true;
     rtkit.enable = true;
   };
 }
