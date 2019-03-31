4eeecc1d37d2fc3e5ba0d68b3ebf776e1935fb34
livmarieg <34752102+livmarieg@users.noreply.github.com>
2019-03-31 10:22:12 -0500

    Update fileSystems.nix

--- fileSystems.nix
+++ fileSystems.nix
@@ -3,13 +3,13 @@
 {
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/7f0c054f-3cdc-4e46-8716-5b8a20b217f0";
+      device = "/dev/disk/by-uuid/498a394c-8e3a-4595-bbfd-424332dbfda1";
       fsType = "ext4";
       options = [ "noatime" "nodiratime" "discard" ];
     };
 
     "/boot" = { 
-      device = "/dev/disk/by-uuid/5A00-F9A8";
+      device = "/dev/disk/by-uuid/3844-0071";
       fsType = "vfat";
     };
   };
