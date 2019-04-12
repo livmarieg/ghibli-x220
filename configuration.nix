f74ac7b190acfc69dbdbbb642a02f2e54e5f0101
cvoges12 <vogesclayton@gmail.com>
2019-04-12 12:47:41 -0500

    annet-common and import.nix

--- configuration.nix
+++ configuration.nix
@@ -4,19 +4,7 @@
   imports = [ 
     <nixos-hardware/lenovo/thinkpad/x220>
     ./boot.nix
-    ./common/boot.nix
-    ./common/environment.nix
-    ./common/hardware.nix
-    ./common/i18n.nix
-    ./common/networking.nix
-    ./common/nix.nix
-    ./common/nixpkgs.nix
-    ./common/powerManagement.nix
-    ./common/programs.nix
-    ./common/security.nix
-    ./common/services.nix
-    ./common/system.nix
-    ./common/users.nix
+    ./common/import.nix
     ./docs.nix
     ./environment.nix
     ./fonts.nix
